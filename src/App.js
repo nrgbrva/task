6035e3364f0d3f17bf56521190abdebf5006d8fa
nurgun ganbarova <114482997+nrgbrva@users.noreply.github.com>
2022-11-21 15:56:42 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+const dil = "javascript";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>nurgun ganbarova</h1>
+      <p>{dil} is awesome!!!</p>
+    </div>
+  );
+}
